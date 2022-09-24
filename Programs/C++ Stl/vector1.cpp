8e134d234aa62dff311bb72ddec83648dfd6c9e6
Raahul-Aanand <duffer.rdx@gmail.com>
2022-09-24 23:36:39 +0530

    Final Changes

--- Programs/C++ Stl/vector1.cpp
+++ Programs/C++ Stl/vector1.cpp
@@ -6,11 +6,13 @@ int main(){
   vector<int> v;
   for(int i=0 ;i<6 ;i++){
     v.push_back(i);
+    //or we can use emplace_back to pudh the element in vector
+    v.emplace_back(i);
   }
   cout<<"vector is :";
   for(vector<int>::iterator it = v.begin() ; it != v.end() ;++it){
     cout<<*it;
-    cout<<' \n '; 
+    cout<<endl;
   }
 
 }
